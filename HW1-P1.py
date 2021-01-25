c0e3120cf90f3ed6c6537dc2f8eb4ca9ae66515d
Rick_AL <77567737+rick-ativit@users.noreply.github.com>
2021-01-25 11:30:04 +0700

    Add files via upload

--- HW1-P1.py
+++ HW1-P1.py
@@ -9,25 +9,33 @@ d_random = start + (end - start) * R.random()
 #Convert datetime -> str
 var1 = d.strftime("%Y-%m-%d %H:%M:%S:%f")
 var2 = d_random.strftime("%Y-%m-%d %H:%M:%S:%f")
+var2i= R.randint(0,99999)
 
 #Step 2
 f=open('CurrentDateTime.txt','w')
 f.write(var1) 
-f.write(var2) 
+f.write(var2)
+f.write(str(var2i))
 f.close()
 
 #Step 3
 f=open('CurrentDateTime.txt')
-s1=f.readline(26) #Read
-s2=f.readline()
+s1=f.readline(26) #Current datetime
+s2=f.readline(26) #Random datetime
+s2i=f.readline() #Random integer
 f.close()
 
 #Step 4
 #Convert str -> datetime
-s1d = datetime.strptime(s1,"%Y-%m-%d %H:%M:%S:%f")
-s2d = datetime.strptime(s2,"%Y-%m-%d %H:%M:%S:%f") 
-print(s1d) #Print current date
-print(s2d) #Print random date
+s1d = datetime.strptime(s1,"%Y-%m-%d %H:%M:%S:%f") #Current datetime format
+s1i = int(s1[-6:]) #Current, milliseconds, int
+s2d = datetime.strptime(s2,"%Y-%m-%d %H:%M:%S:%f") #Random datetime format
+s2i = int(s2i) #Random, milliseconds, int
+print("Current datetime: "+str(s1d)) #Print current date
+print("Random datetime: "+str(s2d)) #Print random date
+print("Random integer: "+str(s2i)) #Print random int
 DeltaT = s1d - s2d
-print(DeltaT) #Print time difference
+DeltaT2 = s1i - s2i
+print("DeltaT from current & random datetime: ",DeltaT) #Print time difference
+print("DeltaT from current time & int: 0.",DeltaT2)
 
